94d3ad8d3e549c7b3eca27c9315c258119a4e673
calculus <kalanah1996@gmail.com>
2019-06-22 19:00:43 +0530

    third commit

--- src/seproject/SEProject.java
+++ src/seproject/SEProject.java
@@ -16,7 +16,7 @@ public class SEProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HELLO");
+        System.out.println("HELLO WORLD");
     }
     
 }
